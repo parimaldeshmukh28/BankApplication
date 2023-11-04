7f7c103588abdfa5ce4cf07db65bf064c67e62b7
parimal123 <parimal.d.deshmukh>
2023-11-04 21:40:33 +0530

    customer details

---
+++ src/main/java/com/banking/bankApplication/model/Customer.java
@@ -0,0 +1,60 @@
+package com.banking.bankApplication.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customerdetails")
+public class Customer {
+	@Id
+	@Column(name = "cid")
+	private int cid;
+
+	private String name;
+
+	private String address;
+
+	private String email;
+
+	public int getCid() {
+		return cid;
+	}
+
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [cid=" + cid + ", name=" + name + ", address=" + address + ", email=" + email + "]";
+	}
+	
+	
+
+}
